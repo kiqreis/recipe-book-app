58b1497056777140901a7ce52b997f39914b28db
Kaique Reis <kreis1265@gmail.com>
2024-09-27 02:28:40 -0300

    feat: add global exception filter for handling errors with localization support

---
+++ src/Backend/MyRecipeBook.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,40 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionBase;
+
+namespace MyRecipeBook.Api.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+  public void OnException(ExceptionContext context)
+  {
+    if (context.Exception is MyRecipeBookException)
+    {
+      HandleProjectException(context);
+    }
+    else
+    {
+      ThrowUnknownException(context);
+    }
+  }
+
+  private void HandleProjectException(ExceptionContext context)
+  {
+    if (context.Exception is RequestValidationException)
+    {
+      var exception = context.Exception as RequestValidationException;
+      
+      context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+      context.Result = new BadRequestObjectResult(new ErrorResponse(exception!.ErrorMessages));
+    }
+  }
+
+  private void ThrowUnknownException(ExceptionContext context)
+  {
+    context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+    context.Result = new ObjectResult(new ErrorResponse(ResourceMessagesException.UNKNOWN_ERROR));
+  }
+}
