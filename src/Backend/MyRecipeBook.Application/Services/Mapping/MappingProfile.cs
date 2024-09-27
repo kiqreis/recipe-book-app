ddb597181efe721ad63b99a38a721652f3f696cb
Kaique Reis <kreis1265@gmail.com>
2024-09-27 02:12:29 -0300

    feat: implement user mapping for database interactions

---
+++ src/Backend/MyRecipeBook.Application/Services/Mapping/MappingProfile.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Application.Services.Mapping;
+
+public class MappingProfile : Profile
+{
+  public MappingProfile()
+  {
+    CreateMap<CreateUserRequest, User>()
+      .ForMember(target => target.Password, opt => opt.Ignore())
+      .ReverseMap();
+    
+    CreateMap<CreateUserResponse, User>().ReverseMap();
+  }
+}
