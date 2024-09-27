21cd9886e3826b134cfe851adbab5f1cc179359d
Kaique Reis <kreis1265@gmail.com>
2024-09-27 02:07:22 -0300

    chore: implement temporary security layer with SHA-512 hashing

---
+++ src/Backend/MyRecipeBook.Application/SecurityConfig/PasswordEncrypt.cs
@@ -0,0 +1,28 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MyRecipeBook.Application.SecurityConfig;
+
+public class PasswordEncrypt
+{
+  public string Encrypt(string password)
+  {
+    var bytes = Encoding.ASCII.GetBytes($"{password}f2m_+Vm3cV*j");
+    var hashBytes = SHA512.HashData(bytes);
+
+    return StringBytes(hashBytes);
+  }
+
+  private static string StringBytes(byte[] bytes)
+  {
+    var sb = new StringBuilder();
+
+    foreach (byte b in bytes)
+    {
+      var hex = b.ToString("x2");
+      sb.Append(hex);
+    }
+
+    return sb.ToString();
+  } 
+}
