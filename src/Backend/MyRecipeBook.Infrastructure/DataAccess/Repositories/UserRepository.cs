03a276b0bbb1a3b40bf891691d761dd71905be05
Kaique Reis <kreis1265@gmail.com>
2024-09-27 02:23:22 -0300

    feat: implement database access methods in infrastructure for user management

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Repositories.UserRepository;
+
+namespace MyRecipeBook.Infrastructure.DataAccess.Repositories;
+
+public class UserRepository(AppDbContext context) : IUserRepository
+{
+  public async Task Add(User user) => await context.Users.AddAsync(user);
+
+  public async Task<bool> IsActiveUserWithEmail(string email) => await context
+    .Users.AnyAsync(u => u.Email.Equals(email) && u.IsActive);
+}
