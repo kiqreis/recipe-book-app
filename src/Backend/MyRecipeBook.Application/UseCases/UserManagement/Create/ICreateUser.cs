b5ae885ae77d0e3fcf0e56d0f4a25c3c7f673454
Kaique Reis <kreis1265@gmail.com>
2024-09-30 02:09:19 -0300

    feat: add ICreateUser interface for user creation use case

---
+++ src/Backend/MyRecipeBook.Application/UseCases/UserManagement/Create/ICreateUser.cs
@@ -0,0 +1,9 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.Application.UseCases.UserManagement.Create;
+
+public interface ICreateUser
+{
+  public Task<CreateUserResponse> Execute(CreateUserRequest request);
+}
