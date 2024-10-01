0f6f2e0965581f91b5bc265b61d02e4e92be16a7
Kaique Reis <kreis1265@gmail.com>
2024-10-01 15:44:10 -0300

    feat: adding passwordencrypt to the di container

---
+++ src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Application.SecurityConfig;
+using MyRecipeBook.Application.UseCases.UserManagement.Create;
+
+namespace MyRecipeBook.Application;
+
+public static class DependencyInjectionExtension
+{
+  public static void AddApplication(this IServiceCollection services)
+  {
+    AddAutoMapper(services);
+    AddPasswordEncrypt(services);
+    AddUseCases(services);
+  }
+
+  private static void AddAutoMapper(IServiceCollection services)
+  {
+    services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    //AppDomain.CurrentDomain.GetAssemblies()
+  }
+
+  private static void AddUseCases(IServiceCollection services)
+  {
+    services.AddScoped<ICreateUser, CreateUser>();
+  }
+
+  private static void AddPasswordEncrypt(IServiceCollection services)
+  {
+    services.AddScoped<PasswordEncrypt>();
+  }
+}
