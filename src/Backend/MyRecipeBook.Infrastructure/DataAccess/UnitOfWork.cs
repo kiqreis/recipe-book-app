6b1e271d46f66f2f7d51496024a8ddd6698461c0
Kaique Reis <kreis1265@gmail.com>
2024-10-01 15:39:53 -0300

    feat: implement Unit of Work pattern for transaction management

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using MyRecipeBook.Domain.Repositories;
+
+namespace MyRecipeBook.Infrastructure.DataAccess;
+
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+  public async Task CommitAsync() => await context.SaveChangesAsync();
+
+  public void Dispose() => context.Dispose();
+}
