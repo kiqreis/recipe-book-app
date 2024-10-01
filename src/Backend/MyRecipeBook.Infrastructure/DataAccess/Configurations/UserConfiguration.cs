a4c772b11cdd4a6c611e12db58090488aa4ac8fa
Kaique Reis <kreis1265@gmail.com>
2024-10-01 15:38:20 -0300

    feat: add UserConfiguration implementing IEntityTypeConfiguration

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/Configurations/UserConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Infrastructure.DataAccess.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+  public void Configure(EntityTypeBuilder<User> builder)
+  {
+    builder.ToTable("Users");
+
+    builder.Property(u => u.Id)
+      .HasColumnName("Id")
+      .HasColumnType("int")
+      .UseIdentityColumn();
+
+    builder.Property(u => u.IsActive)
+      .HasColumnName("IsActive")
+      .HasColumnType("bit")
+      .HasDefaultValue(true);
+
+    builder.Property(u => u.CreateAt)
+      .HasColumnName("CreateAt")
+      .HasColumnType("datetime")
+      .HasDefaultValueSql("getdate()");
+
+    builder.Property(u => u.Name)
+      .HasColumnName("Name")
+      .HasColumnType("nvarchar")
+      .HasMaxLength(120)
+      .IsRequired();
+
+    builder.Property(u => u.Email)
+      .HasColumnName("Email")
+      .HasColumnType("varchar")
+      .HasMaxLength(160)
+      .IsRequired();
+
+    builder.Property(u => u.Password)
+      .HasColumnName("Password")
+      .HasColumnType("nvarchar")
+      .HasMaxLength(64)
+      .IsRequired();
+  }
+}
