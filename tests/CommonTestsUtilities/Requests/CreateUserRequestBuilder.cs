a9bbf1e5b35d4da233c1d8f14ac987b453d0983c
Kaique Reis <kreis1265@gmail.com>
2024-10-07 18:36:00 -0300

    test: add test for user creation

---
+++ tests/CommonTestsUtilities/Requests/CreateUserRequestBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Requests;
+
+namespace CommonTestsUtilities.Requests;
+
+public class CreateUserRequestBuilder
+{
+  public static CreateUserRequest Build()
+  {
+    return new Faker<CreateUserRequest>()
+      .RuleFor(user => user.Name, (f) => f.Person.FirstName)
+      .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name))
+      .RuleFor(user => user.Password, (f) => f.Internet.Password());
+  }
+}
---
+++ tests/Validators.Test/UserTests/Create/CreateUserValidatorTest.cs
@@ -0,0 +1,17 @@
+﻿using CommonTestsUtilities.Requests;
+using MyRecipeBook.Application.UseCases.UserManagement.Create;
+
+namespace Validators.Test.UserTests.Create;
+
+public class CreateUserValidatorTest
+{
+  [Fact]
+  public void Success()
+  {
+    var validator = new CreateUserValidator();
+    var request = CreateUserRequestBuilder.Build();
+    var result = validator.Validate(request);
+
+    Assert.True(result.IsValid);
+  }
+}
