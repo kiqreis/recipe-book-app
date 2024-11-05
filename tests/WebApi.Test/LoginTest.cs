f9600cddf2161631e637f76b72a2f46f71315183
Kaique Reis <kreis1265@gmail.com>
2024-11-05 17:50:29 -0300

    test: add integration test for login controller error message validation

--- tests/WebApi.Test/LoginTest.cs
+++ tests/WebApi.Test/LoginTest.cs
@@ -1,8 +1,12 @@
-﻿using FluentAssertions;
+﻿using CommonTestsUtilities.Requests;
+using FluentAssertions;
 using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exceptions;
+using System.Globalization;
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
+using WebApi.Test.InlineData;
 
 namespace WebApi.Test;
 
@@ -33,5 +37,31 @@ public class LoginTest(CustomWebApplicationFactory factory) : IClassFixture<Cust
 
     responseData.RootElement.GetProperty("name").GetString().Should().NotBeNullOrWhiteSpace()
       .And.Be(_name);
+  }
+
+  [Theory]
+  [ClassData(typeof(CultureInlineData))]
+  public async Task Error_Login_Invalid(string culture)
+  {
+    var request = LoginUserRequestBuilder.Build();
+
+    if (_httpClient.DefaultRequestHeaders.Contains("Accept-Language"))
+    {
+      _httpClient.DefaultRequestHeaders.Remove("Accept-Language");
+    }
+
+    _httpClient.DefaultRequestHeaders.Add("Accept-Language", culture);
+
+    var response = await _httpClient.PostAsJsonAsync(_method, request);
+
+    response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+
+    await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+    var responseData = await JsonDocument.ParseAsync(responseBody);
+    var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+    var expectedMessage = ResourceMessagesException.ResourceManager.GetString("EMAIL_OR_PASSWORD_INVALID", new CultureInfo(culture));
+
+    errors.Should().ContainSingle().And.Contain(e => e.GetString()!.Equals(expectedMessage));
   }
 }
