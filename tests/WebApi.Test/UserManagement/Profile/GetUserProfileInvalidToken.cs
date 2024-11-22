4c472fb1d358c7e055dcb2a8d45ff90a9d5526e2
Kaique Reis <kreis1265@gmail.com>
2024-11-21 23:57:14 -0300

    test: add integration test for token mismatch in user data retrieval

--- tests/WebApi.Test/UserManagement/Profile/GetUserProfileInvalidToken.cs
+++ tests/WebApi.Test/UserManagement/Profile/GetUserProfileInvalidToken.cs
@@ -1,5 +1,35 @@
-﻿namespace WebApi.Test.UserManagement.Profile;
+﻿using CommonTestsUtilities.Token;
+using FluentAssertions;
+using System.Net;
 
-public class GetUserProfileInvalidToken
+namespace WebApi.Test.UserManagement.Profile;
+
+public class GetUserProfileInvalidToken(CustomWebApplicationFactory factory) : MyRecipeBookClassFixture(factory)
 {
+  private readonly string method = "user";
+
+  [Fact]
+  public async Task Error_Token_Invalid()
+  {
+    var response = await Get(method, token: "invalidToken");
+
+    response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+  }
+
+  [Fact]
+  public async Task Error_Without_Token()
+  {
+    var response = await Get(method, token: string.Empty);
+
+    response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+  }
+
+  [Fact]
+  public async Task Error_Token_With_User_NotFound()
+  {
+    var token = JwtTokenGeneratorBuilder.Build().Generate(Guid.NewGuid());
+    var response = await Get(method, token);
+
+    response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+  }
 }
