45894cb6c6fa59e8e1635c7518f2eb526c7b54f5
Kaique Reis <kreis1265@gmail.com>
2024-11-26 16:53:55 -0300

    test: add unit test for user update endpoint

---
+++ tests/CommonTestsUtilities/Requests/UpdateUserRequestBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Requests;
+
+namespace CommonTestsUtilities.Requests;
+
+public class UpdateUserRequestBuilder
+{
+  public static UpdateUserRequest Build()
+  {
+    return new Faker<UpdateUserRequest>()
+      .RuleFor(user => user.Name, (f) => f.Person.FirstName)
+      .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name));
+  }
+}
