2d596716d3655ec6f738f1d1d165a7b3ad679f61
Kaique Reis <kreis1265@gmail.com>
2024-12-06 01:01:10 -0300

    test: add unit test for user password change validation

---
+++ tests/CommonTestsUtilities/Requests/ChangePasswordRequestBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Requests;
+
+namespace CommonTestsUtilities.Requests;
+
+public class ChangePasswordRequestBuilder
+{
+  public static ChangePasswordRequest Build(int passwordLength = 10)
+  {
+    return new Faker<ChangePasswordRequest>()
+      .RuleFor(u => u.Password, f => f.Internet.Password())
+      .RuleFor(u => u.NewPassword, f => f.Internet.Password(passwordLength));
+  }
+}
---
+++ tests/Validators.Test/ChangePassword/ChangePasswordValidatorTest.cs
@@ -0,0 +1,53 @@
+﻿using CommonTestsUtilities.Requests;
+using FluentAssertions;
+using MyRecipeBook.Application.UseCases.UserManagement.ChangePassword;
+using MyRecipeBook.Exceptions;
+
+namespace Validators.Test.ChangePassword;
+
+public class ChangePasswordValidatorTest
+{
+  [Fact]
+  public void Success()
+  {
+    var validator = new ChangePasswordValidator();
+    var request = ChangePasswordRequestBuilder.Build();
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeTrue();
+  }
+
+  [Theory]
+  [InlineData(1)]
+  [InlineData(2)]
+  [InlineData(3)]
+  [InlineData(4)]
+  [InlineData(5)]
+  public void Error_Password_Invalid(int passwordLength)
+  {
+    var validator = new ChangePasswordValidator();
+    var request = ChangePasswordRequestBuilder.Build(passwordLength);
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+
+    result.Errors.Should().ContainSingle()
+      .And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.PASSWORD_INVALID));
+  }
+
+  [Fact]
+  public void Error_Password_Empty()
+  {
+    var validator = new ChangePasswordValidator();
+    var request = ChangePasswordRequestBuilder.Build();
+
+    request.NewPassword = string.Empty;
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+
+    result.Errors.Should().ContainSingle()
+      .And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.PASSWORD_EMPTY));
+  }
+}
