d3f13a88c5a07995b68aa03af532909b656c5630
Kaique Reis <kreis1265@gmail.com>
2024-12-11 04:56:16 -0300

    test: add unit tests for recipe request validator

---
+++ tests/CommonTestsUtilities/Requests/RecipeRequestBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Enums;
+using MyRecipeBook.Communication.Requests;
+
+namespace CommonTestsUtilities.Requests;
+
+public class RecipeRequestBuilder
+{
+  public static RecipeRequest Build()
+  {
+    return new Faker<RecipeRequest>()
+      .RuleFor(recipe => recipe.Title, f => f.Lorem.Word())
+      .RuleFor(recipe => recipe.CookingTime, f => f.PickRandom<CookingTime>())
+      .RuleFor(recipe => recipe.Difficulty, f => f.PickRandom<Difficulty>());
+  }
+}
---
+++ tests/Validators.Test/Recipe/RecipeValidatorTest.cs
@@ -0,0 +1,88 @@
+﻿using CommonTestsUtilities.Requests;
+using FluentAssertions;
+using MyRecipeBook.Application.UseCases.Recipe;
+using MyRecipeBook.Communication.Enums;
+using MyRecipeBook.Exceptions;
+
+namespace Validators.Test.Recipe;
+
+public class RecipeValidatorTest
+{
+  [Fact]
+  public void Success()
+  {
+    var validator = new RecipeValidator();
+    var request = RecipeRequestBuilder.Build();
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeTrue();
+  }
+
+  [Fact]
+  public void Error_Cooking_Time_Not_Supported()
+  {
+    var validator = new RecipeValidator();
+    var request = RecipeRequestBuilder.Build();
+
+    request.CookingTime = (CookingTime?)100;
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.COOKING_TIME_NOT_SUPPORTED));
+  }
+
+  [Fact]
+  public void Error_Difficulty_Level_Not_Supported()
+  {
+    var validator = new RecipeValidator();
+    var request = RecipeRequestBuilder.Build();
+
+    request.Difficulty = (Difficulty?)100;
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.DIFFICULTY_LEVEL_NOT_SUPPORTED));
+  }
+
+  [Fact]
+  public void Error_Empty_Title()
+  {
+    var validator = new RecipeValidator();
+    var request = RecipeRequestBuilder.Build();
+
+    request.Title = string.Empty;
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.RECIPE_TITLE_EMPTY));
+  }
+
+  [Fact]
+  public void Success_When_Cooking_Time_Is_Null()
+  {
+    var validator = new RecipeValidator();
+    var request = RecipeRequestBuilder.Build();
+
+    request.CookingTime = null;
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeTrue();
+  }
+
+  [Fact]
+  public void Success_When_Difficulty_Is_Null()
+  {
+    var validator = new RecipeValidator();
+    var request = RecipeRequestBuilder.Build();
+
+    request.Difficulty = null;
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeTrue();
+  }
+}
