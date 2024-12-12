642890e205793e00d0a00f21958a2cfb5643e326
Kaique Reis <kreis1265@gmail.com>
2024-12-12 17:28:50 -0300

    test: add unit tests for recipe request validator with complex types

--- src/Shared/MyRecipeBook.Exceptions/ResourceMessagesException.Designer.cs
+++ src/Shared/MyRecipeBook.Exceptions/ResourceMessagesException.Designer.cs
@@ -61,6 +61,24 @@ namespace MyRecipeBook.Exceptions {
         }
         
         /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Note that at least one ingredient is required.
+        /// </summary>
+        public static string AT_LEAST_ONE_INGREDIENT {
+            get {
+                return ResourceManager.GetString("AT_LEAST_ONE_INGREDIENT", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Please ensure there is at least one instruction provided for the recipe.
+        /// </summary>
+        public static string AT_LEAST_ONE_INSTRUCTION {
+            get {
+                return ResourceManager.GetString("AT_LEAST_ONE_INSTRUCTION", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Consulta uma cadeia de caracteres localizada semelhante a The chosen dish type is not supported.
         /// </summary>
         public static string COOKING_TIME_NOT_SUPPORTED {
@@ -84,6 +102,15 @@ namespace MyRecipeBook.Exceptions {
         public static string DIFFICULTY_LEVEL_NOT_SUPPORTED {
             get {
                 return ResourceManager.GetString("DIFFICULTY_LEVEL_NOT_SUPPORTED", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a The chosen dish type is not supported.
+        /// </summary>
+        public static string DISH_TYPE_NOT_SUPPORTED {
+            get {
+                return ResourceManager.GetString("DISH_TYPE_NOT_SUPPORTED", resourceCulture);
             }
         }
         
@@ -124,6 +151,33 @@ namespace MyRecipeBook.Exceptions {
         }
         
         /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a The ingredient is left empty, please provide a valid value.
+        /// </summary>
+        public static string INGREDIENT_EMPTY {
+            get {
+                return ResourceManager.GetString("INGREDIENT_EMPTY", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a The instruction is left empty, please provide a valid instruction.
+        /// </summary>
+        public static string INSTRUCTION_EMPTY {
+            get {
+                return ResourceManager.GetString("INSTRUCTION_EMPTY", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a The instruction exceeds the maximum limit of 255 characters.
+        /// </summary>
+        public static string INSTRUCTION_EXCEEDS_LIMIT_CHARACTERS {
+            get {
+                return ResourceManager.GetString("INSTRUCTION_EXCEEDS_LIMIT_CHARACTERS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Consulta uma cadeia de caracteres localizada semelhante a The name field cannot be empty.
         /// </summary>
         public static string NAME_EMPTY {
@@ -138,6 +192,15 @@ namespace MyRecipeBook.Exceptions {
         public static string NO_TOKEN {
             get {
                 return ResourceManager.GetString("NO_TOKEN", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Use a non-negative value for the instruction step.
+        /// </summary>
+        public static string NON_NEGATIVE_INSTRUCTION_STEP {
+            get {
+                return ResourceManager.GetString("NON_NEGATIVE_INSTRUCTION_STEP", resourceCulture);
             }
         }
         
@@ -165,6 +228,15 @@ namespace MyRecipeBook.Exceptions {
         public static string RECIPE_TITLE_EMPTY {
             get {
                 return ResourceManager.GetString("RECIPE_TITLE_EMPTY", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a When adding instructions, please ensure that each one has a unique step number. Two or more instructions cannot have the same step.
+        /// </summary>
+        public static string TWO_OR_MORE_INSTRUCTIONS_SAME_ORDER {
+            get {
+                return ResourceManager.GetString("TWO_OR_MORE_INSTRUCTIONS_SAME_ORDER", resourceCulture);
             }
         }
         
--- tests/Validators.Test/Recipe/RecipeValidatorTest.cs
+++ tests/Validators.Test/Recipe/RecipeValidatorTest.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using MyRecipeBook.Application.UseCases.Recipe;
 using MyRecipeBook.Communication.Enums;
 using MyRecipeBook.Exceptions;
+using System.Globalization;
 
 namespace Validators.Test.Recipe;
 
@@ -84,5 +85,116 @@ public class RecipeValidatorTest
     var result = validator.Validate(request);
 
     result.IsValid.Should().BeTrue();
+  }
+
+  [Fact]
+  public void Success_When_DishTypes_Is_Empty()
+  {
+    var request = RecipeRequestBuilder.Build();
+
+    request.DishTypes.Clear();
+
+    var validator = new RecipeValidator();
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeTrue();
+  }
+
+  [Fact]
+  public void Error_Invalid_DishTypes()
+  {
+    var request = RecipeRequestBuilder.Build();
+
+    request.DishTypes.Add((DishType)100);
+
+    var validator = new RecipeValidator();
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.DISH_TYPE_NOT_SUPPORTED));
+  }
+
+  [Fact]
+  public void Error_Empty_Ingredients()
+  {
+    var request = RecipeRequestBuilder.Build();
+
+    request.Ingredients.Clear();
+
+    var validator = new RecipeValidator();
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.AT_LEAST_ONE_INGREDIENT));
+  }
+
+  [Fact]
+  public void Error_Empty_Instructions()
+  {
+    var request = RecipeRequestBuilder.Build();
+
+    request.Instructions.Clear();
+
+    var validator = new RecipeValidator();
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.AT_LEAST_ONE_INSTRUCTION));
+  }
+
+  [Fact]
+  public void Error_Empty_Value_Ingredients()
+  {
+    var request = RecipeRequestBuilder.Build();
+
+    request.Ingredients.Add(string.Empty);
+
+    var validator = new RecipeValidator();
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.INGREDIENT_EMPTY));
+  }
+
+  [Fact]
+  public void Error_Same_Step_Instruction()
+  {
+    var request = RecipeRequestBuilder.Build();
+
+    request.Instructions.First().Step = request.Instructions.Last().Step;
+
+    var validator = new RecipeValidator();
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.TWO_OR_MORE_INSTRUCTIONS_SAME_ORDER));
+  }
+
+  [Fact]
+  public void Error_Negative_Step_Instruction()
+  {
+    var request = RecipeRequestBuilder.Build();
+
+    request.Instructions.First().Step = -(new Random().Next(1, 100));
+
+    var validator = new RecipeValidator();
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.NON_NEGATIVE_INSTRUCTION_STEP));
+  }
+
+  [Fact]
+  public void Error_Empty_Value_Instruction()
+  {
+    var request = RecipeRequestBuilder.Build();
+
+    request.Instructions.First().Text = string.Empty;
+
+    var validator = new RecipeValidator();
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.INSTRUCTION_EMPTY));
   }
 }
