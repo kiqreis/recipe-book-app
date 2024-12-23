ee373eb7422dbca1961bc8dc92a5ba3c6d1bcc3e
Kaique Reis <kreis1265@gmail.com>
2024-12-22 21:28:00 -0300

    test: add unit tests for recipe filter validation

---
+++ tests/CommonTestsUtilities/Requests/RecipeFilterRequestBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Enums;
+using MyRecipeBook.Communication.Requests;
+
+namespace CommonTestsUtilities.Requests;
+
+public class RecipeFilterRequestBuilder
+{
+  public static RecipeFilterRequest Build()
+  {
+    return new Faker<RecipeFilterRequest>()
+      .RuleFor(user => user.CookingTimes, f => f.Make(1, () => f.PickRandom<CookingTime>()))
+      .RuleFor(user => user.Difficulties, f => f.Make(1, () => f.PickRandom<Difficulty>()))
+      .RuleFor(user => user.DishTypes, f => f.Make(1, () => f.PickRandom<DishType>()))
+      .RuleFor(user => user.RecipeTitleIngredient, f => f.Lorem.Word());
+  }
+}
---
+++ tests/Validators.Test/Recipe/Filter/RecipeFilterValidatorTest.cs
@@ -0,0 +1,62 @@
+﻿using CommonTestsUtilities.Requests;
+using FluentAssertions;
+using MyRecipeBook.Application.UseCases.RecipeManagement.Filter;
+using MyRecipeBook.Communication.Enums;
+using MyRecipeBook.Exceptions;
+
+namespace Validators.Test.Recipe.Filter;
+
+public class RecipeFilterValidatorTest
+{
+  [Fact]
+  public void Success()
+  {
+    var validator = new FilterRecipeValidator();
+    var request = RecipeFilterRequestBuilder.Build();
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeTrue();
+  }
+
+  [Fact]
+  public void Error_Invalid_Cooking_Time()
+  {
+    var validator = new FilterRecipeValidator();
+    var request = RecipeFilterRequestBuilder.Build();
+
+    request.CookingTimes.Add((CookingTime)100);
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.COOKING_TIME_NOT_SUPPORTED));
+  }
+
+  [Fact]
+  public void Error_Invalid_Difficulty()
+  {
+    var validator = new FilterRecipeValidator();
+    var request = RecipeFilterRequestBuilder.Build();
+
+    request.Difficulties.Add((Difficulty)100);
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.DIFFICULTY_LEVEL_NOT_SUPPORTED));
+  }
+
+  [Fact]
+  public void Error_Invalid_DishType()
+  {
+    var validator = new FilterRecipeValidator();
+    var request = RecipeFilterRequestBuilder.Build();
+
+    request.DishTypes.Add((DishType)100);
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.DISH_TYPE_NOT_SUPPORTED));
+  }
+}
