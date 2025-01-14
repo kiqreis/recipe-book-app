345ef52d1cbe3e218bafa3950e72f6251f81a550
Kaique Reis <kreis1265@gmail.com>
2025-01-14 15:57:14 -0300

    test: add unit test to validate recipe generation

---
+++ tests/CommonTestsUtilities/Requests/RecipeGenerateRequestBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Requests;
+
+namespace CommonTestsUtilities.Requests;
+
+public class RecipeGenerateRequestBuilder
+{
+  public static RecipeGeneratedRequest Build(int count = 6)
+  {
+    return new Faker<RecipeGeneratedRequest>()
+    .RuleFor(user => user.Ingredients, f => f.Make(count, () => f.Commerce.ProductName()));
+  }
+}
+
---
+++ tests/Validators.Test/Recipe/Generate/RecipeGenerateValidatorTest.cs
@@ -0,0 +1,77 @@
+﻿using CommonTestsUtilities.Requests;
+using FluentAssertions;
+using MyRecipeBook.Application.UseCases.RecipeManagement.Generate;
+using MyRecipeBook.Domain.ValueObjects;
+using MyRecipeBook.Exceptions;
+
+namespace Validators.Test.Recipe.Generate;
+
+public class RecipeGenerateValidatorTest
+{
+  [Fact]
+  public void Success()
+  {
+    var validator = new RecipeGenerateValidator();
+    var request = RecipeGenerateRequestBuilder.Build();
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeTrue();
+  }
+
+  [Fact]
+  public void Error_More_Maximum_Ingredients()
+  {
+    var validator = new RecipeGenerateValidator();
+    var request = RecipeGenerateRequestBuilder.Build(MyRecipeBookRuleConstants.MAXIMUM_INGREDIENTS_RECIPE_GENERATE + 1);
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.INVALID_INGREDIENTS_NUMBER));
+  }
+
+  [Fact]
+  public void Error_Duplicated_Ingredients()
+  {
+    var validator = new RecipeGenerateValidator();
+    var request = RecipeGenerateRequestBuilder.Build(count: 4);
+
+    request.Ingredients.Add(request.Ingredients[0]);
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.DUPLICATED_INGREDIENTS_IN_LIST));
+  }
+
+  [Fact]
+  public void Error_Empty_Ingredient()
+  {
+    var validator = new RecipeGenerateValidator();
+    var request = RecipeGenerateRequestBuilder.Build(count: 1);
+
+    request.Ingredients.Add(string.Empty);
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.INGREDIENT_EMPTY));
+  }
+
+  [Fact]
+  public void Error_Ingredient_Not_Following_Pattern()
+  {
+    var validator = new RecipeGenerateValidator();
+    var request = RecipeGenerateRequestBuilder.Build(count: 4);
+
+    request.Ingredients.Add("This is a invalid ingredient because is too long");
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.INGREDIENT_NOT_FOLLOWING_PATTERN));
+  }
+}
