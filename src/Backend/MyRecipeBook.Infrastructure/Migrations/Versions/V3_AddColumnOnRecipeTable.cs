b875e18dadf1cd1ac92dc194375232818694f590
Kaique Reis <kreis1265@gmail.com>
2025-01-14 18:18:25 -0300

    feat: add ImageId column to Recipes table

---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/V3_AddColumnOnRecipeTable.cs
@@ -0,0 +1,12 @@
+﻿using FluentMigrator;
+
+namespace MyRecipeBook.Infrastructure.Migrations.Versions;
+
+[Migration(3, "Add column on recipe table to save or update images")]
+public class V3_AddColumnOnRecipeTable : VersionBase
+{
+  public override void Up()
+  {
+    Alter.Table("Recipes").AddColumn("ImageId").AsString().Nullable();
+  }
+}
